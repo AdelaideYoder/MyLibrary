e36b7e426b16aea1feb43f2fd0539cb8fc2be8b8
Adelaide <adelaide.yoder@gmail.com>
2018-10-11 10:29:25 -0500

    added in patron model

--- MyLibrary/Models/Patron.cs
+++ MyLibrary/Models/Patron.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,16 @@ namespace MyLibrary.Models
 {
     public class Patron
     {
+        [Key]
+        public int PatronId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Display(Name = "Checked out books")]
+        public int BookId { get; set; }
+
+        public Book book { get; set; }
     }
 }
