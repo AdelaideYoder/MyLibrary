a77f144f4f95293475452261241d3242dca0a8f0
Adelaide <adelaide.yoder@gmail.com>
2018-10-11 10:37:49 -0500

    added in LibraryCreateViewModel

--- MyLibrary/ViewModels/LibraryCreateViewModel.cs
+++ MyLibrary/ViewModels/LibraryCreateViewModel.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MyLibrary.Data;
+using MyLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace MyLibrary.ViewModels
 {
     public class LibraryCreateViewModel
     {
+        public Library Library { get; set; }
+
     }
 }
